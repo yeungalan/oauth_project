b83301702c903b439d680fef1642eefec23ae9fb
SuperUser <yeungbluecat123@gmail.com>
2017-07-28 19:34:01 +0800

    Update config.php

--- config.php
+++ config.php
@@ -6,9 +6,9 @@ $github_client_url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVE
 $google_api_url = "5318022020-80fbc5pcgvf52gq2el63b33tolcitkop.apps.googleusercontent.com"; //Google OAuth Api Key
 $telegram_bot_id = "300531451:AAHNEgy9O0tZ2z5kKkIJt8TOGLDfzPN16Wk"; //Telegram Bot ID (Format : xxxxxxx:xxxxxxxxxxxxxx)
 
-$announcement = "系統正在進行更新工作，閣下的登入可能會較平常緩慢。不便之處，敬請見諒。";
-$announcementgrade = ""; //success , info , warning , danger or keep "" for not displaying
+$announcement = "Hello World!";
+$announcementgrade = "info"; //success , info , warning , danger or keep "" for not displaying
 
-$requireauthenction = "true"; //false = allow all internet user use this services , true = require key for using this system
-$authenctionkey = "12345678"
+$requireauthenction = "false"; //false = allow all internet user use this services , true = require key for using this system
+$authenctionkey = "" //authenction password
 ?>
