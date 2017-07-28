7226c3ade617b79fb8ae29de8c17bce1d1435c74
SuperUser <yeungbluecat123@gmail.com>
2017-07-28 18:57:57 +0800

    V2.2.0

--- config.php
+++ config.php
@@ -1,8 +1,14 @@
 ﻿<?php
 $user_json_name = "1nbfa.json"; //使用者儲存位置
 $github_client_id = "0982c21b9c3db83c40b9"; //Github OAuth Api Key
-$github_clint_secret = "e618aee8256aa36feba9aa02e71777f25ebb3eb9"; //Github OAuth Api Key
-$github_client_url = "https://oauth.alanyeung.co/github.php"; //GIthub Endpoint , type http[s]://your_server_address/github.php
+$github_clint_secret = "e618aee8256aa36feba9aa02e71777f25ebb3eb9"; //Github OAuth Api Keys
+$github_client_url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]/github.php";
 $google_api_url = "5318022020-80fbc5pcgvf52gq2el63b33tolcitkop.apps.googleusercontent.com"; //Google OAuth Api Key
 $telegram_bot_id = "300531451:AAHNEgy9O0tZ2z5kKkIJt8TOGLDfzPN16Wk"; //Telegram Bot ID (Format : xxxxxxx:xxxxxxxxxxxxxx)
+
+$announcement = "系統正在進行更新工作，閣下的登入可能會較平常緩慢。不便之處，敬請見諒。";
+$announcementgrade = ""; //success , info , warning , danger or keep "" for not displaying
+
+$requireauthenction = "true"; //false = allow all internet user use this services , true = require key for using this system
+$authenctionkey = "12345678"
 ?>
--- index.php
+++ index.php
@@ -33,24 +33,86 @@ include "config.php";
 
   <br>
   <?php
-  if($announcement==""){
+    if($requireauthenction=="true"){
+		if($authenctionkey==$_REQUEST["key"]){
+		}else{
+			header("Refresh: 0; url=error.php?from=index.php&error=Access Key Invaild");
+		}
+	}else{
+	}
+  
+  if($announcement=="" || $announcementgrade==""){
   }else{
 	  echo '<div class="alert alert-'.$announcementgrade.'" role="alert"><i class="fa fa-asterisk fa-2" aria-hidden="true"></i> '.$announcement.'</div>';
   }
   ?>
+  	<?php
+	$json = file_get_contents("developer.json");
+	$obj = json_decode($json);
+	if(isset($_REQUEST["dev_key"])){
+		$dev = $_REQUEST["dev_key"];
+	}else{
+		$dev = "null";
+	}
+	?>
   <br>
   
-  <div class="ts slate">
+ <div class="ts heading vertically padded slate">
   <p>以下程式或網站正嘗試登入</p>
  
-  <p><?php echo $_REQUEST["url"]; ?></p>
+
+  <details class="ts accordion" open>
+    <summary>
+        <i class="dropdown icon"></i>  
+		<?php 
+  if(strpos($_REQUEST["url"],"127.0.0.1")===False){
+  echo $_REQUEST["url"];
+  }else{
+  echo "本地端應用程式";
+  
+  }
+  ?>
+  		<?php 
+		$domain = parse_url($_REQUEST["url"]);
+		if($obj->{$dev}->{"verified"} == "true"){
+			if($domain["host"] == $obj->{$dev}->{"usualdomain"} || $domain["host"] == "127.0.0.1"  || $domain["host"] == "localhost")
+			echo '<i class="fa fa-shield" aria-hidden="true" ></i>';
+		}
+		?>
+    </summary>
+    <div class="content">
+        <p>開發者 : <?php echo $obj->{$dev}->{"author"}; ?></p>
+		<p>作者網頁 : <?php echo $obj->{$dev}->{"web"}; ?></p>
+		<p>認證日期 : <?php echo $obj->{$dev}->{"date"}; ?></p>
+    </div>
+</details>
 </div>
 <br>
   <div class="ts fluid vertical buttons">
-  	<a  href="main.php?url=<?php echo bin2hex($_REQUEST["url"]); ?>" class="ts negative button" id="main">Main Server</a>
-    <a  href="google.php?url=<?php echo bin2hex($_REQUEST["url"]); ?>" class="ts warning button">Google</a>
-    <a  href="github.php?redirect=1&url=<?php echo bin2hex($_REQUEST["url"]); ?>" class="ts primary button">Github</a>
-    <a  href="imus.php?url=<?php echo bin2hex($_REQUEST["url"]); ?>" class="ts inverted button">IMUS</a>
+  <?php 
+    $disable=0;
+  if(strpos($_REQUEST["disable"],"M")===False){
+  echo '<a  href="main.php?url='.bin2hex($_REQUEST["url"]).'" class="ts negative button" id="main">Main Server</a>';
+  $disable =  $disable +1;
+  }
+  if(strpos($_REQUEST["disable"],"G")===False){
+  echo '<a  href="google.php?url='.bin2hex($_REQUEST["url"]).'" class="ts warning button">Google</a>';
+    $disable =  $disable +1;
+  }
+  if(strpos($_REQUEST["disable"],"T")===False){
+  echo '<a  href="github.php?url='.bin2hex($_REQUEST["url"]).'" class="ts primary button">Github</a>';
+    $disable =  $disable +1;
+  }
+  if(strpos($_REQUEST["disable"],"I")===False){
+  echo '<a  href="imus.php?url='.bin2hex($_REQUEST["url"]).'" class="ts inverted button">IMUS</a>';
+    $disable =  $disable +1;
+  }	
+  
+  if($disable ==0){
+ header("Refresh: 0; url=error.php?from=index.php&error=No Login Method Available");
+  }	
+  ?>
+  	
 </div>
    
 </div>
