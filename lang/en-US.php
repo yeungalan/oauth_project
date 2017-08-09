f88ebae0816073112f93adaa35496c4618bfd85c
SuperUser <yeungbluecat123@gmail.com>
2017-08-09 17:39:02 +0800

    Beta Version for en-US

---
+++ lang/en-US.php
@@ -0,0 +1,84 @@
+<?php
+/* 
+-----------------
+Language: English
+-----------------
+*/
+
+$lang = array();
+
+//index.php
+$lang['index_title'] = 'The following program or website is trying to sign in';
+$lang['index_application'] = 'Local Application';
+$lang['index_dev'] = 'Developer : ';
+$lang['index_author'] = 'Developer website : ';
+$lang['index_date'] = 'Authorized date : ';
+$lang['index_mainserver'] = 'Main server';
+$lang['index_google'] = 'Google';
+$lang['index_github'] = 'Github';
+$lang['index_imus'] = 'IMUS';
+
+//error.php
+$lang['error_header'] = 'Oh No! Error Occured :(';
+$lang['error_desc'] = 'Error Description :';
+$lang['error_url'] = 'Error URL : '; 
+$lang['error_det'] = 'Error details';
+$lang['error_err'] = 'Error';
+$lang['error_debug'] = 'Debug message';
+
+//2FA.php
+$lang['fa_authmethod'] = 'Please select your verification method';
+$lang['fa_tg'] = 'Telegram :';
+$lang['fa_waitconfirm'] = 'Waiting confirmation';
+$lang['fa_redirwait'] = 'Do not close this page. The system will automatically redirect after verification is complete.';
+$lang['fa_err'] = 'Error';
+$lang['fa_invaildcode'] = 'Code invaild';
+$lang['fa_gauth'] = 'Two-step verification(Google Authenticator) :';
+$lang['fa_gauth6'] = 'Please open Google Authenticator and enter the six-digit code that you see';
+$lang['fa_auth30'] = 'Please note that the code is valid for only 30 seconds. Please enter it as soon as possible.';
+$lang['fa_code'] = 'Six-digit code';
+$lang['fa_title'] = 'Two-step login verification';
+$lang['fa_complete'] = 'Confirmation completed';
+$lang['fa_red'] = 'Do not close this page. The system will automatically redirect now.';
+$lang['fa_fail'] = 'Fail';
+$lang['fa_retry'] = 'Retry again';
+$lang['fa_tg1'] = 'Two-step login, IP';
+$lang['fa_tg2'] = 'is logging in, if you confirm that the relevant login is issued by you, please press OK, otherwise press refused';
+$lang['fa_tgcon'] = 'confirm';
+$lang['fa_tgrej'] = 'reject';
+$lang['fa_tgconunicode'] = 'confirm'; //in unicode if require
+$lang['fa_tgregunicode'] = 'reject';
+
+//github.php , google.php
+$lang['auth_logout'] = 'logout';
+$lang['auth_ok'] = 'login';
+
+//imus.php
+$lang['imus_err'] = 'Error';
+$lang['imus_errd'] = 'IMUS return username or password is incorrect';
+$lang['imus_ok'] = 'Success';
+$lang['imus_okd'] = 'Please wait, the system will automatically redirect now.';
+$lang['imus_title'] = 'Using IMUS system login';
+$lang['imus_username'] = 'Username';
+$lang['imus_pwd'] = 'Password';
+$lang['imus_reset'] = 'Reset';
+$lang['imus_submit'] = 'Submit';
+
+//newuser.php , newuser_tg.php , newuser_final.php
+$lang['newuser_google'] = 'Google';
+$lang['newuser_googledec'] = 'Enter your Google Authenticator verification code';
+$lang['newuser_telegram'] = 'Telegram';
+$lang['newuser_telegramdec'] = 'Enter your telegram verification';
+$lang['newuser_confirm'] = 'comfirm';
+$lang['newuser_newuser'] = 'new user';
+$lang['newuser_6code'] = 'Six-digit code';
+$lang['newuser_keepcode'] = 'Please Save your code.';
+$lang['newuser_lanuch'] = 'Lanuch';
+$lang['newuser_enter'] = 'Input this string in chatting windows : ';
+$lang['newuser_enterdes'] = 'The system will automatically detect your identity, after completion you will be redirected to the final step';
+$lang['newuser_skip'] = 'If you do not have Telegram, please click here.';
+$lang['newuser_final'] = 'Please make sure the following information is correct and then press confirm.';
+
+//location.php
+$lang['location_title'] = 'The program is positioning your location';
+$lang['location_des'] = 'This page will be redirected upon completion. If you do not agree that the site uses your location, you may not be able to log in properly。';
